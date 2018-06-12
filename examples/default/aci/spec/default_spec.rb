cd3e04fe00e6f9d60abce4951cb5693253c0a70d
Thomas Steinbach <thomas.steinbach@aikq.de>
2018-06-12 08:48:02 +0200

    currently in refactoring

--- examples/default/aci/spec/default_spec.rb
+++ examples/default/aci/spec/default_spec.rb
@@ -1,18 +1,8 @@
-describe package('docker-engine') do
-  it { should be_installed }
-end
-
-describe package('docker-py') do
-  it { should be_installed.by('pip') }
-end
+require 'spec_helper'
 
 describe service('docker') do
   it { should be_running }
   it { should be_enabled }
-end
-
-describe user('vagrant') do
-  it { should belong_to_group 'docker' }
 end
 
 # check installation of docker and the alias as well
--- examples/remote_config/aci/inspec/default.rb
+++ examples/remote_config/aci/inspec/default.rb
@@ -1,12 +1,16 @@
 describe service('docker') do
+  it { should be_installed }
+  it { should be_enabled }
   it { should be_running }
 end
 
 describe port(4243) do
-  it { should be_listening.with('tcp6') }
+ it { should be_listening }
+ its('protocols') { should cmp 'tcp6' }
 end
 
 # check 'remote' access on localhost
 describe command('docker --tlsverify --tlscacert="$HOME/.ansible-data/thomass.docker/pki/ca.pem" --tlscert="$HOME/.ansible-data/thomass.docker/pki/client-cert.pem" --tlskey="$HOME/.ansible-data/thomass.docker/pki/client-key.pem" -H=tcp://localhost:4243 version') do
-  its(:stderr) { should eq '' }
+  its('stderr') { should eq '' }
 end
+
